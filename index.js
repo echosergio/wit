af4f50639405bbdcf3cddb9fa0700c7da8e267e2
Sergio Vicente de las Heras <s.vicente@payvision.com>
2020-01-10 16:33:49 +0100

    WIP: esm module

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
-const files = require('./lib/files');
-const inquirer = require('./lib/inquirer');
+require = require('esm')(module);
+
+import chalk from 'chalk';
+import clear from 'clear';
+import figlet from 'figlet';
+import inquirer from './lib/inquirer';
+require('./lib/cli').cli(process.argv);
 
 clear();
-
-console.log(chalk.yellow(figlet.textSync('Wit', { horizontalLayout: 'full' })));
+console.log(chalk.yellow(figlet.textSync('wit', { horizontalLayout: 'full' })));
 
 const run = async () => {
   const credentials = await inquirer.askGithubCredentials();
   console.log(credentials);
 };
-
-run();
